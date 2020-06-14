abb496efd3a6611f7366bb769e54fceafee81021
ShiraPhoo <64349107+ShiraPhoo@users.noreply.github.com>
2020-06-14 15:34:05 +0630

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+
+const selectElement = (element) => {
+    return document.querySelector(element);
+}   
+
+let menuToggle = selectElement(".menu-toggle");
+let body = selectElement("body");
+
+menuToggle.addEventListener("click", ()=>{
+    body.classList.toggle('open');
+});
